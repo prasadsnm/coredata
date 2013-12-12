cd74b86ea873ee6d56f3c57d30bd180fb19bb73d
vara prasad <gdv.prasadg@gmail.com>
2013-12-12 17:48:37 +0530

    created coredata sample project

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreData
+//
+//  Created by MAC on 11/12/13.
+//  Copyright (c) 2013 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CoreDataAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CoreDataAppDelegate class]));
+    }
+}
